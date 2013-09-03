d9bb0d7b3199d6db080ff90c7d97d1c8f8583b6d
apcs <apcs@mvhs-imac04u528.dcsdk12.org>
2013-09-03 08:03:42 -0600

    while and for loop

--- loops.java
+++ loops.java
@@ -1,7 +1,18 @@
 class loops {
 	
 	public static void main(String[] args) {
-		System.out.println("Loops");
+		int x = 1;
+		while (x <= 100) { //stopping condition
+			System.out.println(x); // print x
+			x++; // paradox rectifier			
+		};
+
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		};
+
+
+
 	}
 
 }
