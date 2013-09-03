4d4a4b3ea4c609b3cb535d59ae10f71bd92768c0
apcs <apcs@mvhs-imac04u528.dcsdk12.org>
2013-09-03 08:11:29 -0600

    counting challenge 1

--- loops.java
+++ loops.java
@@ -10,7 +10,16 @@ class loops {
 		for (int i = 1; i <= 100; i++) {
 			System.out.println(i);
 		};
-
+		System.out.println("-----------------------------------");
+		int y = 45;
+		while (y >= 20){
+			System.out.println(y);
+			y = y-5;
+		};
+		System.out.println("-----------------------------------");
+		for (int j = 24; j <= 68; j = j +2) {
+			System.out.println(j);
+		};
 
 
 	}
